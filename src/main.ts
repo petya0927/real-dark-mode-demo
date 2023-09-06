5d6aba2f0cca4e89ac8399f1dde856a661e3ec84
petya0927 <unyipeti@gmail.com>
2023-09-07 00:13:57 +0200

    added vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import realDarkMode from "real-dark-mode";
+import { inject } from '@vercel/analytics';
+
+inject();
 
 document.getElementById("toggle")!.addEventListener("click", () => {
   realDarkMode();
